8a803184abd450350682437384f3748a87079e7f
leomarcov <32820131+leomarcov@users.noreply.github.com>
2022-09-07 10:16:11 +0200

    Update install_w7.ps1

--- windows/install_w7.ps1
+++ windows/install_w7.ps1
@@ -2,21 +2,21 @@
 # Install WMF 5.1: https://docs.microsoft.com/es-es/powershell/scripting/windows-powershell/wmf/setup/install-configure?view=powershell-7.2
 
 
-# ENABLE TLS 1.2
+# W7 only: ENABLE TLS 1.2
 # https://www.delftstack.com/howto/powershell/installing-the-nuget-package-in-powershell/
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 [Net.ServicePointManager]::SecurityProtocol
 # Restart PowerShell
 
-
-Install-Module -Name Posh-SSH -Force
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+Install-Module -Name Posh-SSH -Force
 New-SSHSession -ComputerName 10.0.2.15 -Port 58889 -AcceptKey -Credential alumno 
 New-SSHSession -ComputerName 10.0.2.15 -Port 58889 -AcceptKey -Credential alumno -KeyFile 'c:\windows\...'
 
 # CREATE JOB SCHEDULE
 Unregister-ScheduledJob labadmin-script_server-agent
 $agent_path="C:\ProgramData\labadmin-script_server_agent\labadmin-script_server_agent.ps1"
-set-executionpolicy remotesigned
 Register-ScheduledJob -Trigger (New-JobTrigger -AtStartup -RandomDelay 00:01:00) -FilePath $agent_path -Name labadmin-script_server-agent
+
+
