b43058e49b5f23e16b3675b39917b706b335905b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-05 09:32:45 +0100

    Create update.ps1

---
+++ windows/update.ps1
@@ -0,0 +1,12 @@
+#Requires -RunAsAdministrator
+
+#===============================================================================
+#  GLOBAL CONFIG VARIABLES
+#===============================================================================
+$agent_path="${ENV:ProgramFiles}\labadmin\labadmin-script_server_agent"
+
+$url="https://raw.githubusercontent.com/leomarcov/labadmin-script_server_agent/main/windows"
+Invoke-WebRequest -Uri ($url+"/labadmin-script_server_agent.ps1") -OutFile ($agent_path+"\labadmin-script_server_agent.ps1")
+Invoke-WebRequest -Uri ($url+"/install.ps1") -OutFile ($agent_path+"\install.ps1")
+Invoke-WebRequest -Uri ($url+"/uninstall.ps1") -OutFile ($agent_path+"\uninstall.ps1")
+Invoke-WebRequest -Uri ($url+"/update.ps1") -OutFile ($agent_path+"\update.ps1")
