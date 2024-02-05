4bde9ec20d06299b773d57b304c9db637d3ad84a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-05 09:55:00 +0100

    Update update.ps1

--- windows/update.ps1
+++ windows/update.ps1
@@ -5,6 +5,7 @@
 #===============================================================================
 $agent_path="${ENV:ProgramFiles}\labadmin\labadmin-script_server_agent"
 
+Write-Host "`nDownloading script files ..." -ForegroundColor Green
 $url="https://raw.githubusercontent.com/leomarcov/labadmin-script_server_agent/main/windows"
 Invoke-WebRequest -Uri ($url+"/labadmin-script_server_agent.ps1") -OutFile ($agent_path+"\labadmin-script_server_agent.ps1")
 Invoke-WebRequest -Uri ($url+"/install.ps1") -OutFile ($agent_path+"\install.ps1")
