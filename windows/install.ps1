e0896119c9d95be893c8393ab2f75705d9559357
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 16:16:21 +0100

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -18,7 +18,7 @@
 #===============================================================================
 $agent_path="${ENV:ProgramFiles}\labadmin\labadmin-script_server_agent"
 $agent_data="${ENV:ALLUSERSPROFILE}\labadmin\labadmin-script_server_agent"
-$agent_file="${agent_path}\labadmin-script_server_agent.ps1"
+$agent_file="${agent_path}\lss-agent.ps1"
 $agent_user="labadmin"
 
 
