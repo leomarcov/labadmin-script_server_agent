2ac94dfcd46b0dadbba934ea326d549672af75f6
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-18 08:20:36 +0100

    Update update.ps1

--- windows/update.ps1
+++ windows/update.ps1
@@ -7,7 +7,8 @@ $agent_path="${ENV:ProgramFiles}\labadmin\labadmin-script_server_agent"
 
 Write-Host "`nDownloading script files ..." -ForegroundColor Green
 $url="https://raw.githubusercontent.com/leomarcov/labadmin-script_server_agent/main/windows"
-Invoke-WebRequest -Uri ($url+"/labadmin-script_server_agent.ps1") -OutFile ($agent_path+"\labadmin-script_server_agent.ps1")
+Invoke-WebRequest -Uri ($url+"/lss-agent.ps1") -OutFile ($agent_path+"\lss-agent.ps1")
+Invoke-WebRequest -Uri ($url+"/lss-config-schedule.ps1") -OutFile ($agent_path+"\lss-config-schedule.ps1")
 Invoke-WebRequest -Uri ($url+"/install.ps1") -OutFile ($agent_path+"\install.ps1")
 Invoke-WebRequest -Uri ($url+"/uninstall.ps1") -OutFile ($agent_path+"\uninstall.ps1")
 Invoke-WebRequest -Uri ($url+"/update.ps1") -OutFile ($agent_path+"\update.ps1")
