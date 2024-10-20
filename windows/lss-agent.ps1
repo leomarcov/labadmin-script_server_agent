8d900b5d6d2b3aa43a930c7d7c32d24fa70b2e53
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-20 11:55:34 +0200

    Update lss-agent.ps1

--- windows/lss-agent.ps1
+++ windows/lss-agent.ps1
@@ -123,10 +123,7 @@ $script_list -Replace '(?m)^(?=.)', '  - '
 Write-Output "`n`nEXECUTING SCRIPTS..."
 
 #### GET AND EXEC SCRIPTS
-ForEach ($script in $($script_list -split "`r`n")) {
-	# Check if save script.ps1 and script.log files on $scripts_path
- 	$nosave_script = $script -match '$script -match '\[NOSAVE\]''
-    
+ForEach ($script in $($script_list -split "`r`n")) {   
 	Write-Output "`n_______________________________________________________________________________________________________________"
 	Write-Output "SCRIPT: $script"
 
@@ -174,6 +171,8 @@ ForEach ($script in $($script_list -split "`r`n")) {
     }
 
  	# REMOVE SCRIPT AND LOG
+  	# Check if no save script.ps1 and script.log files on $scripts_path
+ 	$nosave_script = $script -match '$script -match '\[NOSAVE\]''
   	if($nosave_script) {
    		Remove-Item -Force -LiteralPath $script_path
 		Remove-Item -Force -LiteralPath $script_log
