d1f01d1a67a19c8b3226821a825b1866d46aabfd
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-11-05 21:27:57 +0100

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,6 +1,5 @@
 #Requires -RunAsAdministrator
 
-#!/usr/bin/env bash
 #===================================================================================
 # LABADMIN SCRIPT SERVER AGENT INSTALL FOR WINDOWS
 #         FILE: install.ps1
